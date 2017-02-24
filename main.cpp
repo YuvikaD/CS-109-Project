a284dc01ac9b27d6adcd4f16a17945cf9c9436cb
RyanJaime <jaimeryan139@yahoo.com>
2017-02-24 13:38:07 -0800

    Update main.cpp
    changed file parsing so it 
    Stores a Rule's name into a Map Key
    Stores a Rule's right hand side "predicates" into a vector of vectors of strings.
    one of these smaller vector will have for example... Father, $X, $Y

--- main.cpp
+++ main.cpp
@@ -8,11 +8,14 @@ int main(){
 	vector<Fact> vFact; // use map instead? map of vectors that hold objects, multiple mother objects will go into one vector.
 	string line;		// empty string to load the input file's lines into
 	string leftHandSide;
+	string rightHandSide;
+	string predName;
 	string separator;
 	string logop;	
 	ifstream readFile("input.txt");	// open the input file
 	string name;		// the name of the Fact or Rule, like "Father"
 	string data;		// the params of the Fact, like "Allen"
+	string preds;
 	vector<string> predicates; 
 	while(getline(readFile,line)){	// read from input file, put contents into 'line' string
 		stringstream iss(line);		// put contents of line into a ss object
@@ -26,15 +29,22 @@ int main(){
 			getline(iss,logop,' ');	// get logop
 			Rule * r = new Rule(logop);
 			Rule_map[name] = r;
-			while(getline(iss,data,' ')){ // add contents to the Fact object's vector, separated by ','
+			int i=0;
+			char a = 'a';
+			while(getline(iss,data,' ')){
 				stringstream iss4(data);
-				getline(iss4,SOMETHING)
-			
-				predicates.push_back(data);
-				//Rule_map[name]->vstring.push_back(data);
-				Rule_map[name]->paramVector.push_back(predicates);
-				
+				i++;
+				getline(iss4,predName,'(');
+				vector<string> first_vector;
+				first_vector.push_back(predName);
+				(getline(iss4,rightHandSide,')'));
+				stringstream iss5(rightHandSide);
+				while(getline(iss5,preds,',')){
+					first_vector.push_back(preds);
+				}
+				r->paramVector.push_back(first_vector);
 			}
+			cout<< i << endl;
 		} else {	// Fact
 			getline(iss3,name,'(');	
 			Fact * f = new Fact(name);		// make a fact object, it's parameter is what we read in, ex: "Father"
