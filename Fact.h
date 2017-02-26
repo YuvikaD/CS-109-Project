667f875d397c29b61f2816ce06cff16b68ac9fba
RyanJaime <jaimeryan139@yahoo.com>
2017-02-25 21:39:56 -0800

    Update Fact.h

--- Fact.h
+++ Fact.h
@@ -1,15 +1,16 @@
 #include "common.h"
+#include "Manipulator.h"
 #ifndef Fact_H
 #define Fact_H
+
 using namespace std;
 class Fact {
-	string title;	
-		// private data member
+	string title;
 	public:
 	vector<string> vstring;
 	
 	vector<string> get_vstring();
-	void printFact();
+	void dumpFacts(Manipulator M);
 	void query(string s);
 	friend ostream& operator<<(ostream &os,Fact * fact);
 	
