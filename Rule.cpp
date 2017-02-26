af0f4e71ca0c748b56fd7bdf230153dcd2ee1d53
RyanJaime <jaimeryan139@yahoo.com>
2017-02-25 21:42:04 -0800

    Update Rule.cpp

--- Rule.cpp
+++ Rule.cpp
@@ -2,19 +2,17 @@
 #include "Rule.h"
 using namespace std;
 Rule::Rule(string log){// constructor 
-	//title = rule;
 	if (log == "OR"){logop = 0;}
 	else if (log == "AND"){logop = 1;}
 	else {cout << " constructor error: log was not AND or OR" << endl;}
-	vector<vector<string>> paramVector(100);
-	
+	vector<vector<string>> paramVector(100);	
 }
 
 ostream& operator<< (std::ostream &os, Rule* rule)
 {
 	for(vector<string> i : rule->paramVector){
 		for(string s : i){
-			cout << s << " ";
+			os << s << " ";
 		}
 	}
     return os;
