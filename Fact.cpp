f6ea283d49394d783427a08f0a729c9032b18adf
RyanJaime <jaimeryan139@yahoo.com>
2017-02-25 21:40:27 -0800

    Update Fact.cpp

--- Fact.cpp
+++ Fact.cpp
@@ -10,26 +10,21 @@ vector<string> Fact::get_vstring(){
 	return vstring;
 }
 
-void Fact::printFact(){
-	//cout << title << ": ";
-	for (vector<string>::iterator it = vstring.begin() ; it != vstring.end(); ++it){ 
-		cout << *it << " ";
-	}
-	cout << endl;
-}
-
 ostream& operator<< (std::ostream &os, Fact* fact)
 {
+	os << fact->title << ": ";
 	for(string i : fact->vstring){
-			cout << i << " ";
+		if(!i.compare("|")){
+			os << endl << fact->title << ": ";
+		} else	os << i << " ";
 	}
+	os << endl;
     return os;
 }
 
 void query(map<string,Fact*> fmap,string s){
 	if(fmap.count(s) == 1){
-		cout << s << ": " << endl
-		<< fmap[s] << endl;
+		cout << s << ": " << fmap[s] << endl;
 	}
 	else {
 		cout << "The fact named " << s << " is not in here" << endl;
