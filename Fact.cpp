22e1be5136038fc673a78bd897592a015a7bc28d
sedmond831 <shawn.tf2@gmail.com>
2017-02-26 15:36:36 -0800

    updated fact output
    fact now prints in correct format

--- Fact.cpp
+++ Fact.cpp
@@ -12,13 +12,18 @@ vector<string> Fact::get_vstring(){
 
 ostream& operator<< (std::ostream &os, Fact* fact)
 {
-	os << fact->title << ": ";
-	for(string i : fact->vstring){
+	os << fact->title << "(";
+	for(int n=0; n<fact->vstring.size(); n++){
+		string i = fact->vstring[n];
+		string m;
+		if(n+1 != (fact->vstring.size())) m = fact->vstring[n+1];
 		if(!i.compare("|")){
-			os << endl << fact->title << ": ";
-		} else	os << i << " ";
+			os << ")" << endl << fact->title << "(";
+		}else if(!m.compare("|") || n+1 == fact->vstring.size()){
+			os << i;
+		}else os << i << ",";
 	}
-	os << endl;
+	os << ")" << endl;
     return os;
 }
 
