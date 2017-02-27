433b9ec6989d4bc9961099e0d418aabd7ed20cb3
YuvikaD <ydube@ucsc.edu>
2017-02-26 17:35:59 -0800

    Update Rule.h

--- Rule.h
+++ Rule.h
@@ -3,13 +3,15 @@
 #define Rule_H
 using namespace std;
 class Rule {
+		// private data member
 	bool logop;
 	public:
 	vector<vector<string>> paramVector;
-	//vector<string> get_vstring();
 	bool get_logop();
 	void printRule();
+	void Rquery(map<string,Rule*> rmap,string s);
 	Rule(string log);	// constructor
 	~Rule();
+	void check(map<string,Rule*> rmap,map<string,Fact*> fmap, string x, string y);
 };
 #endif
