2369660f43545b6928e7ce21b41e1968599dfff6
RyanJaime <jaimeryan139@yahoo.com>
2017-03-01 15:45:41 -0800

    updated LOAD added inference(works for facts only)

--- Manipulator.cpp
+++ Manipulator.cpp
@@ -1,13 +1,12 @@
-#include "common.h"
 #include "Manipulator.h"
 using namespace std;
 Manipulator:: Manipulator(){}	// constructor
-Manipulator::~Manipulator(){}	// destructor 
+Manipulator::~Manipulator(){}	// constructor 
 
 void Manipulator::drop(string name){
 	if(Fact_map.find(name)!=Fact_map.end()) Fact_map.erase(name);
 	if(Rule_map.find(name)!=Rule_map.end()) Rule_map.erase(name);
-	cout <<'dropped' << endl;		
+	cout << "dropped" << endl;		
 }
 
 void Manipulator::dumpFacts(ostream &os){
@@ -15,12 +14,10 @@ for(auto it = Fact_map.begin(); it != Fact_map.end(); ++it) {
 		os << it->second;
     }
 }
+
 void Manipulator::dumpRules(ostream &os){
-	bool first = 1;
-	auto it = Rule_map.begin();
-	os << it->second << endl;
-	for(it = Rule_map.begin(); it != Rule_map.end(); ++it) {
-		os << it->second->get_logop() << " " << it->second << endl;
+	for(auto it = Rule_map.begin(); it != Rule_map.end(); ++it) {
+		os << it->second;
     }
 }
 void Manipulator::dump(){
@@ -28,9 +25,25 @@ void Manipulator::dump(){
 	fout.open("output.txt");
 	dumpFacts(fout);
 	dumpRules(fout);
-}	
-void Manipulator::load(){
-ifstream readFile("input.txt");
+}
+
+void Manipulator::inference(string filename){
+	ifstream readFile(filename);
+	getline(readFile,line);
+	stringstream iss(line);
+	getline(iss,leftHandSide,'(');
+	if(Fact_map.count(leftHandSide) == 1){
+		cout << Fact_map[leftHandSide];
+	}
+	if(Rule_map.count(leftHandSide) == 1){
+		//cout << Rule_map[leftHandSide];
+		//Yuvi's code 
+	}
+	
+}
+
+void Manipulator::load(string filename){
+ifstream readFile(filename);
 while(getline(readFile,line)){	// read from input file, put contents into 'line' string
 		stringstream iss(line);		// put contents of line into a ss object
 		getline(iss,leftHandSide,')');	// read from ss object, put contents into 'leftHandSide' string until ')' char is read
@@ -39,25 +52,52 @@ while(getline(readFile,line)){	// read from input file, put contents into 'line'
 		stringstream iss3(leftHandSide);
 		
 		if(separator == ":-"){	// Rule
-			getline(iss2,name,':');	
+			getline(iss2,name,':');
 			getline(iss,logop,' ');	// get logop
-			vector<string> first_vector;
+			vector<vector<string>> p_vector;
 			stringstream iss6(name);
 			getline(iss6,name3,'(');
-			first_vector.push_back(name3);
-			//cout << name3 << endl;
-			Rule * r = new Rule(logop);	
-			Rule_map[name3] = r;
-			
 			getline(iss6,temp2,')');
 			stringstream iss5(temp2);
-			while (getline(iss5,temp,',')){
-				first_vector.push_back(temp);
+
+			if(Rule_map.count(name3) != 1){			
+				vector<string> first_vector;
+				Rule * r = new Rule(logop);	
+				Rule_map[name3] = r;
+				first_vector.push_back(name3);				
+				while (getline(iss5,temp,',')){				
+					first_vector.push_back(temp);			
+				}
+				p_vector.push_back(first_vector);
+				while(getline(iss,data,' ')){
+				stringstream iss4(data);
+				getline(iss4,predName,'(');
+				vector<string> second_vector;
+				second_vector.push_back(predName);
+				(getline(iss4,rightHandSide,')'));
+				stringstream iss5(rightHandSide);
+				while(getline(iss5,preds,',')){
+					second_vector.push_back(preds);
+				}	
+				p_vector.push_back(second_vector);			
+			}
+			r->RuleVector.push_back(p_vector);
 			}
 			
-			
-			r->paramVector.push_back(first_vector); // put vector in big vector
-			while(getline(iss,data,' ')){
+			else if(Rule_map.count(name3) == 1){
+				vector<string> first_vector;
+				vector<string> space_vector;
+				vector<vector<string>> space_vector2;
+				space_vector.push_back(space);
+				space_vector2.push_back(space_vector);
+				
+				
+				first_vector.push_back(name3);				
+				while (getline(iss5,temp,',')){				
+					first_vector.push_back(temp);			
+				}
+				p_vector.push_back(first_vector);
+				while(getline(iss,data,' ')){
 				stringstream iss4(data);
 				getline(iss4,predName,'(');
 				vector<string> second_vector;
@@ -67,13 +107,18 @@ while(getline(readFile,line)){	// read from input file, put contents into 'line'
 				while(getline(iss5,preds,',')){
 					second_vector.push_back(preds);
 				}
-				r->paramVector.push_back(second_vector);
+				
+				
+				p_vector.push_back(second_vector);
 			}
+
+				Rule_map.find(name3)->second->RuleVector.push_back(space_vector2);
+				Rule_map.find(name3)->second->RuleVector.push_back(p_vector);
+		}
 			
 		} else {	// Fact
 			getline(iss3,name,'(');	
 			if(Fact_map.count(name) != 1){ 	// If this Fact is not in the map
-				//cout<< " did not  found " << name << endl;
 				Fact * f = new Fact(name);	// make fact object, parameter ex: "Father"
 				Fact_map[name] = f;
 				while(getline(iss3,data,',')){	// add contents to the Fact object's vector, separated by ','
@@ -82,7 +127,6 @@ while(getline(readFile,line)){	// read from input file, put contents into 'line'
 			}
 			
 			else if(Fact_map.count(name) == 1){ // If this Fact name already exists in the map
-				//cout<< " did found " << name << endl;
 				Fact_map.find(name)->second->vstring.push_back(space);
 				while(getline(iss3,data,',')){	// add contents to the Fact object's vector, separated by ','
 					Fact_map.find(name)->second->vstring.push_back(data);
